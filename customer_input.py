9e2142daafb88ea77dcbc927a2e553f086e7b973
amita987 <agiri134@gmail.com>
2026-07-01 00:53:10 +0530

    Create customer_input.py

---
+++ customer_input.py
@@ -0,0 +1,21 @@
+# ==========================
+# IMPORT LIBRARIES
+# ==========================
+
+import streamlit as st
+
+
+
+# ==========================
+# CUSTOMER INPUT FUNCTION
+# ==========================
+
+
+def get_customer_inputs():
+
+
+    st.header(
+
+        "Customer Configuration"
+
+    )
