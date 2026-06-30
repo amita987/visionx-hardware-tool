b5fb0d32894de7d488633465450c3f47aa771c0e
amita987 <agiri134@gmail.com>
2026-06-30 19:22:23 +0530

    Create color_config.py

---
+++ color_config.py
@@ -0,0 +1,12 @@
+# ==========================
+# TABLE COLOR SETTINGS
+# ==========================
+
+
+HEADER_COLOR = "#1F4E79"
+
+EXPLANATION_COLOR = "#D9EAF7"
+
+BUTTON_COLOR = "#28A745"
+
+TEXT_COLOR = "#000000"
